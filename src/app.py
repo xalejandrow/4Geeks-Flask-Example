622cd7bc92170c90548dc9f5adaf2f493e052ce8
Alew <xalejandrow@gmail.com>
2022-08-05 23:25:51 +0000

    creamos primer archivo Flask

--- src/app.py
+++ src/app.py
@@ -1,2 +1,10 @@
 from flask import Flask
 
+app = Flask(__name__) # hace referencia al nombre del archivo
+
+@app.route('/')
+def hello_flask():
+    return 'Hello Flask'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1', port=5000)
