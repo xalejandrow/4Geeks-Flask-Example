3fd675d841e9a2fe2ccfb9ff1eefc007a53c0f8d
Alew <xalejandrow@gmail.com>
2022-08-06 00:00:28 +0000

    agregando variables para pasar por url al flask

--- src/app.py
+++ src/app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__) # hace referencia al nombre del archivo
 
 @app.route('/')
 def hello_flask():
     return 'Hello Flask'
+
+@app.route('/inicio')
+def show_home():
+    return render_template('index.html')
+
+@app.route('/url_variables/<string:name>/<int:age>')
+def url_variables(name, age):
+    if age < 18:
+        return jsonify(message='Lo siento ' + name + ' no estas autorizado'), 401
+    else:
+        return jsonify(message = ' Bienvenido ' + name ), 200
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1', port=5000)
