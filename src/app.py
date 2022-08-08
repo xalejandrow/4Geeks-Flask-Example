90b856933aeaa172515dbe3668b9a0c6fae2f0c2
Alew <xalejandrow@gmail.com>
2022-08-08 22:20:36 +0000

    finalizamos la API en add.py

--- src/app.py
+++ src/app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, jsonify, request
+import pickle
+import pandas as pd
+import numpy as np
 
 app = Flask(__name__) # hace referencia al nombre del archivo
 
@@ -10,13 +13,27 @@ def hello_flask():
 def show_home():
     return render_template('index.html')
 
-@app.route('/url_variables/<string:name>/<int:age>')
-def url_variables(name, age):
-    if age < 18:
-        return jsonify(message='Lo siento ' + name + ' no estas autorizado'), 401
+@app.route('/<string: country>/<string: variety>/<float: aroma>/<float: aftertaste>/<float: acidity>/<float: body>/<float: balance>/<float: moisture>')
+def result(country, variety, aroma, aftertaste, acidity, body, balance, moisture):
+    cols = ['country_of_origin', 'variety', 'aroma','aftertaste','acidity','body','balance','moisture']
+    data = [country, variety, aroma,aftertaste,acidity,body,balance,moisture]
+    posted = pd.DataFrame(np.array(data).reshape(1,8), columns=cols)
+    loaded_model = pickle.load(open('../models/coffee_model.pkl','rb'))
+    result = loaded_model.predict(posted)
+    test_result = result.tolist()[0]
+    if test_result == 'Yes':
+        return jsonify(message = 'Si es un cafe de primera'), 200
     else:
-        return jsonify(message = ' Bienvenido ' + name ), 200
+        return jsonify(message = 'No es un cafe de primera'), 200
 
+
+
+#@app.route('/url_variables/<string:name>/<int:age>')
+#def url_variables(name, age):
+#    if age < 18:
+#        return jsonify(message='Lo siento ' + name + ' no estas autorizado'), 401
+#    else:
+#        return jsonify(message = ' Bienvenido ' + name ), 200
 
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1', port=5000)
