51439089e5e8a3056c6459128e0bb598654a7f11
Alew <xalejandrow@gmail.com>
2022-08-08 21:56:38 +0000

    modificamos los valores para que de Si

--- src/test.py
+++ src/test.py
@@ -2,14 +2,25 @@ import pickle
 import pandas as pd
 import numpy as np
 
-country = 'Other'
-variety = 'Other'
-aroma = 7.42
-aftertaste = 7.33
-acidity = 7.42
-body = 7.25
-balance = 7.33
-moisture = 0.0
+#No
+#country = 'Other'
+#variety = 'Other'
+#aroma = 7.42
+#aftertaste = 7.33
+#acidity = 7.42
+#body = 7.25
+#balance = 7.33
+#moisture = 0.0
+
+#Si
+country = 'Colombia'
+variety = 'Caturra'
+aroma = 7.83
+aftertaste = 7.67
+acidity = 7.33
+body = 7.67
+balance = 7.67
+moisture = 0.11
 
 cols = ['country_of_origin', 'variety', 'aroma','aftertaste','acidity','body','balance','moisture']
 data = [country, variety, aroma,aftertaste,acidity,body,balance,moisture]
